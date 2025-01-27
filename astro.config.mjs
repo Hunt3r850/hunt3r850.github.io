e031b2dd116b18a6dfbc4ec344d5645aeed55156
H4ck3rHunt <108004077+Hunt3r850@users.noreply.github.com>
2025-01-27 08:40:43 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://hunt3r850.github.io',
     integrations: [
         mdx(),
         sitemap(),
