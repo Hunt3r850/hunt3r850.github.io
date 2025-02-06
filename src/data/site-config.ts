524d2fc6cccb258b92b444b7dadfece717d135c8
H4ck3rHunt <108004077+Hunt3r850@users.noreply.github.com>
2025-02-06 04:22:03 +0100

    update

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -52,7 +52,7 @@ const siteConfig: SiteConfig = {
         },
         {
             text: 'Proyectos',
-            href: '/project'
+            href: '/projects'
         },
         {
             text: 'Github',
